caf00c35d9885f731f20eb916665f5310ceaf052
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2023-11-28 12:11:49 +0100

    fix: minor fixes for UserDetails struct

--- src/features/auth/models/user/user_details.rs
+++ src/features/auth/models/user/user_details.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use serde::Serialize;
 
 #[derive(Debug, Serialize)]
@@ -7,10 +8,12 @@ pub struct UserDetails {
     pub email: String,
     pub username: String,
     pub hash_password: String,
-    pub phone_number: String,
-    pub bio: String,
-    pub profile_url: String,
-    pub user_location: String,
-    pub date_of_birth: String,
-    pub gender: String,
+    pub phone_number: Option<String>,
+    pub bio: Option<String>,
+    pub profile_url: Option<String>,
+    pub user_location: Option<String>,
+    pub date_of_birth: Option<String>,
+    pub gender: Option<String>,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
 }
