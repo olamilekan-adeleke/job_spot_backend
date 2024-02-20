5d65a9153f2ea521ec3d3385efa2c40a2305b2bf
Kod Enigma <olamilekanadeleke123@gmail.com>
2024-02-20 20:16:49 +0100

    feat: save_forgot_password
    Updated the function so that it only update the code in db for a giving user instead of craete another row

--- src/features/auth/db_access/save_forgot_password_code.rs
+++ src/features/auth/db_access/save_forgot_password_code.rs
@@ -6,13 +6,28 @@ pub async fn save_forgot_pasword_code(
     pool: &PgPool,
     data: VerificationData,
 ) -> Result<VerificationCode, BaseError> {
-    let code_data = sqlx::query_as!(
-        VerificationCode,
-        "INSERT into verifications_code (user_id, code, expiration_date) values ($1, $2, $3) returning *",
-        data.user_id, 
-        data.code, 
-        data.expiration_date,
-    ).fetch_one(pool).await?;
+
+    let already_exit = sqlx::query_as!( VerificationCode, "SELECT * from verifications_code where user_id = $1", data.user_id, )
+        .fetch_optional(pool).await?;
+
+
+    let code_data: VerificationCode;
+    if already_exit.is_some() {
+        code_data = sqlx::query_as!(
+            VerificationCode,
+            "UPDATE verifications_code SET code = $1 WHERE user_id = $2 returning *",
+            data.code,
+            data.user_id
+        ).fetch_one(pool) .await?;
+    }else {
+         code_data = sqlx::query_as!(
+            VerificationCode,
+            "INSERT into verifications_code (user_id, code, expiration_date) values ($1, $2, $3) returning *",
+            data.user_id, 
+            data.code, 
+            data.expiration_date,
+        ).fetch_one(pool).await?;
+    }
 
     Ok(code_data)
 }
