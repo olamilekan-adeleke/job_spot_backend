882b0054f7696ed3168e51d631417a343b44e4f7
Kod Enigma <olamilekanadeleke123@gmail.com>
2024-03-25 22:28:33 +0100

    chore: added company data to jop posting

--- src/features/jobs_posting/model/job_posting.rs
+++ src/features/jobs_posting/model/job_posting.rs
@@ -1,6 +1,8 @@
 use chrono::DateTime;
 use serde::Serialize;
 
+use crate::feature::company::models::company::Company;
+
 #[derive(Debug, Serialize)]
 pub struct JobPosting {
     pub job_id: uuid::Uuid,
@@ -10,6 +12,7 @@ pub struct JobPosting {
     pub job_worktype: String,
     pub location: String,
     pub company_id: String,
+    pub company: Company,
     pub job_type: String,
     pub salary: i64,
     pub currency: String,
